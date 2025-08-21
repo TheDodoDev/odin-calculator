28a5c83a259b5dfaf6a22b27315df64bbc7e7a7c
TheDodoDev <100032995+TheDodoDev@users.noreply.github.com>
2025-08-21 11:54:56 -0500

    feat: Add operate function
    - Consolidates all possible calculations into one function

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,5 +1,21 @@
 import '../src/style.css'
 import { add, subtract, multiply, divide } from './operations.ts';
+const ERROR: number = -1;
+const ADD: number = 0;
+const SUB: number = 1;
+const MUL: number = 2;
+const DIV: number = 3;
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
 `
+
+function operate(num1: number, num2: number, operation: number): number {
+    switch (operation) {
+        case ADD: return add(num1, num2);
+        case SUB: return subtract(num1, num2);
+        case MUL: return multiply(num1, num2);
+        case DIV: return divide(num1, num2);
+        default: return ERROR;
+    }
+}
