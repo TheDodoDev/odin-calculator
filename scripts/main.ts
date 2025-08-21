a491bd593208b75d356b3ae31682aae1df432f59
TheDodoDev <100032995+TheDodoDev@users.noreply.github.com>
2025-08-21 11:49:40 -0500

    feat: Add operation functions
    - Functions for addition, subtraction, multiplication, and division

---
+++ scripts/main.ts
@@ -0,0 +1,5 @@
+import '../src/style.css'
+import { add, subtract, multiply, divide } from './operations.ts';
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+
+`
---
+++ scripts/operations.ts
@@ -0,0 +1,22 @@
+function add(addend1: number, addend2: number): number {
+    return addend1 + addend2;
+}
+
+function subtract(minuend: number, subtrahend: number): number {
+    return minuend - subtrahend;
+}
+
+function multiply(multiplicand: number, multiplier: number): number {
+    return multiplicand * multiplier;
+}
+
+function divide(dividend: number, divisor: number): number {
+    return dividend / divisor;
+}
+
+export {
+    add,
+    subtract,
+    multiply,
+    divide
+};
